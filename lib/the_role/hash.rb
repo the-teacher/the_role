5265d4ae26e2f071a546a60e4c5156f5dae6c36e
Ilya N. Zykin <zykin-ilya@ya.ru>
2012-02-27 23:02:24 +0400

    ~~~

--- lib/the_role/hash.rb
+++ lib/the_role/hash.rb
@@ -1,25 +1,24 @@
 class Hash
   def the_reset!(default_value = false)
     base = self
-    base.each do |key, v|
-      if base[key.to_sym].is_a?(Hash)
-        base[key.to_sym] = base[key.to_sym].the_reset!(default_value)
+    base.each do |key, value|
+      if base[key].is_a?(Hash)
+        base[key] = base[key].the_reset!(default_value)
       else
-        base[key.to_sym] = default_value
+        base[key] = default_value
       end
     end
   end
 
-  def the_merge!(hash = nil, default_value = true)
-    return self unless hash.is_a?(Hash)
+  def the_merge! hash
     base = self
-    hash.each do |key, v|
-      if base[key.to_sym].is_a?(Hash) && hash[key.to_sym].is_a?(Hash)
-        base[key.to_sym] = base[key.to_sym].the_merge!(hash[key.to_sym], default_value)
+    hash.each do |key, value|
+      if base[key].is_a?(Hash) && hash[key].is_a?(Hash)
+        base[key] = base[key].the_merge! hash[key]
       else
-        base[key.to_sym] = default_value
+        base[key] = hash[key]
       end
     end
-    base.to_hash
+    base
   end
 end
