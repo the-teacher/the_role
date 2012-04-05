3b1cafe322d59525d4388b8d9f4149aa44204a38
Ilya N. Zykin <zykin-ilya@ya.ru>
2012-04-05 17:53:37 +0400

    views base

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :roles do
       resources :sections, :controller => :role_sections, :only => :none do
+        collection do
+          post :create
+          post :create_rule
+        end
         member do
-          post   :create
-          post   :create_rule
           delete :destroy
           delete :destroy_rule
         end
