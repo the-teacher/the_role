38584dadf2ae40fb1b82cf4c4d8daf79ae3bf11b
John Henderson <jhenderson@anu.ie>
2012-07-26 09:57:48 +0100

    Added css/js assets to precompile path

--- lib/the_role/engine.rb
+++ lib/the_role/engine.rb
@@ -3,6 +3,10 @@ require 'rails'
 
 module TheRole
   class Engine < Rails::Engine
+    initializer "TheRole precompile hook", :group => :all do |app|
+      app.config.assets.precompile += %w( admin_the_role.js admin_the_role.css )
+    end
+
     config.to_prepare do
       Role.send :include, TheRole::RoleModel if the_class_exists? :Role
       User.send :include, TheRole::UserModel if the_class_exists? :User
