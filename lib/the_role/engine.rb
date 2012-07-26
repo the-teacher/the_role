a81b4c27b0a0320a1df74b64bf2e7e316f35d306
John Henderson <jhenderson@anu.ie>
2012-07-26 13:39:59 +0100

    Removed mixin block as it's not wise to assume the user model and application controller is the target

--- lib/the_role/engine.rb
+++ lib/the_role/engine.rb
@@ -6,11 +6,5 @@ module TheRole
     initializer "TheRole precompile hook", :group => :all do |app|
       app.config.assets.precompile += %w( admin_the_role.js admin_the_role.css )
     end
-
-    config.to_prepare do
-      Role.send :include, TheRole::RoleModel if the_class_exists? :Role
-      User.send :include, TheRole::UserModel if the_class_exists? :User
-      ApplicationController.send :include, TheRole::Requires if the_class_exists? :ApplicationController
-    end
   end
 end
