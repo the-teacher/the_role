7106ee4bbe6a7928ea28b3741e95fd5340cd45fe
Ilja Lovrjakov <pman89@ya.ru>
2013-03-25 10:47:42 +0400

    created config section for layout and corresponding generator (rails g the_role install)

---
+++ lib/the_role/config.rb
@@ -0,0 +1,19 @@
+module TheRole
+  def self.configure(&block)
+    yield @config ||= TheRole::Configuration.new
+  end
+
+  def self.config
+    @config
+  end
+
+  # Configuration class
+  class Configuration
+    include ActiveSupport::Configurable
+    config_accessor :layout
+  end
+
+  configure do |config|
+    config.layout = 'the_role'
+  end
+end
