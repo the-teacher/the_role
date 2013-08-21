abff52aeb3ccaa241a76339ea74eaa226ec6f3b4
Jordan Sexton <jordan@jordansexton.com>
2013-08-20 23:20:19 -0500

    Add configuration options accessible at TheRole.config

--- lib/generators/the_role/templates/the_role.rb
+++ lib/generators/the_role/templates/the_role.rb
@@ -1,6 +1,9 @@
-# TheRole.config.param_name => value
+# TheRole.config.param_name = value
 
-TheRole.configure do |config|
-  config.layout            = :application # default Layout for TheRole UI
-  config.default_user_role = nil          # set default role (name)
+TheRole.configure do |c|
+  c.layout            = :application     # default Layout for TheRole UI
+  c.default_user_role = nil              # set default role (name)
+  c.admin_role_name   = 'any_crazy_name' # Admin role name can be changed
+  c.role_class_name   = 'Role'           # Role class can be changed (e.g., 'UserRole')
+  c.role_attribute    = :the_role        # Role attribute/database column can be changed (e.g., :role, :role_hash)
 end
