c08590876e727d1e003c72e478caf1980997d921
Corey Kuwanoe <corey.kuwanoe@gmail.com>
2014-05-02 20:27:00 -0700

    adding dashboard layout id to role

--- app/controllers/admin/roles_controller.rb
+++ app/controllers/admin/roles_controller.rb
@@ -74,7 +74,7 @@ class Admin::RolesController < ApplicationController
   protected
 
   def role_params
-    params.require(:role).permit(:name, :title, :description, :the_role, :based_on_role, :parent_id, :collection_ids => [])
+    params.require(:role).permit(:name, :title, :description, :the_role, :based_on_role, :parent_id, :dashboard_layout_id, :collection_ids => [])
   end
 
   def role_find
